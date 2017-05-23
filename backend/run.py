18263853f239478799115d467da55fcb28b66aba
Ilya Polyakovskiy <ilya.polyakovskiy@jetbrains.com>
2017-05-22 19:40:37 -0700

    Added run-backend script

---
+++ backend/run.py
@@ -0,0 +1,12 @@
+from web_server import WebServer, RestRequestHandler
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option('-H', '--host', dest='host', help='host for web-server (0.0.0.0 by default)')
+parser.add_option('-P', '--port', dest='port', help='port for web-server (80 by default)')
+(options, args) = parser.parse_args()
+host = options.host
+port = int(options.port) if options.port is not None else None
+
+server = WebServer(RestRequestHandler(), host=options.host, port=port)
+server.run()
