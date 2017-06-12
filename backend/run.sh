41871775e5be1c1fc79318adc1581379a1579e5b
Ilya Polyakovskiy <ilya.polyakovskiy@jetbrains.com>
2017-06-12 19:44:47 +0300

    Added running application through daemon utility

--- backend/run.sh
+++ backend/run.sh
@@ -2,4 +2,4 @@
 
 . /home/admin/anaconda3/bin/activate beachranks
 export PYTHONPATH=/home/admin/beachranks/backend
-python /home/admin/beachranks/backend/run_web_server.py
+daemon --name=beachranks -- python /home/admin/beachranks/backend/run_web_server.py
