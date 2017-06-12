1e402e55c37fb847afe838e5fc6e705d5c0f8fc6
Ilya Polyakovskiy <ilya.polyakovskiy@jetbrains.com>
2017-06-12 18:42:45 +0300

    Added conda path in run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-. activate beachranks
+. /home/admin/anaconda3/bin/activate beachranks
 export PYTHONPATH=/home/admin/beachranks/backend
 python /home/admin/beachranks/backend/run_web_server.py
