8f11cd50d5a30323dd8da7e4810c612ca4b9fdef
funduck <qlfunduck@gmail.com>
2017-06-20 00:24:17 +0300

    fixed port for webhook

--- backend/run_bot.py
+++ backend/run_bot.py
@@ -61,10 +61,10 @@ class BotRestRequestHandler:
 
 parser = OptionParser()
 parser.add_option('-H', '--host', dest='host', help='host for web-server (0.0.0.0 by default)')
-parser.add_option('-P', '--port', dest='port', help='port for web-server (8080 by default)')
+parser.add_option('-P', '--port', dest='port', help='port for web-server (88 by default)')
 (options, args) = parser.parse_args()
 host = options.host
-port = int(options.port) if options.port is not None else 8080
+port = int(options.port) if options.port is not None else 88
 
 server = WebServer(BotRestRequestHandler(), host=options.host, port=port)
 server.run()
