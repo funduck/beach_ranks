63485085c78f1b83a0c806d3b0eb0d3220897776
Ilya Polyakovskiy <ilya.polyakovskiy@jetbrains.com>
2017-07-01 12:43:17 +0300

    Enable logging in files on production

--- common/logger.py
+++ common/logger.py
@@ -1,11 +1,17 @@
 import sys
 import logging
 
+import os
+
 
 def init_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    ch = logging.StreamHandler(sys.stdout)
+    if 'LOG_FILE' in os.environ:
+        ch = logging.FileHandler(os.environ['LOG_FILE'])
+    else:
+        ch = logging.StreamHandler(sys.stdout)
+
     ch.setLevel(0)
     formatter = logging.Formatter('%(asctime)s  %(name)s  %(levelname)s  %(message)s')
     ch.setFormatter(formatter)
